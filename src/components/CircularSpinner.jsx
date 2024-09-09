0b85a271b11480770ece86028b2c0d286a6346cf
ngthaoph <ngthaoph@gmail.com>
2024-09-09 22:13:51 +1000

    Update Spinners code

--- src/components/CircularSpinner.jsx
+++ src/components/CircularSpinner.jsx
@@ -20,28 +20,30 @@ const StyledSVG = styled.svg`
   opacity: ${(props) => props.opacity};
 `;
 
-function CircularSpinner({ color, opacity, speed, direction }) {
-  const defaultColor = "#D97292";
-  const defaultOpacity = 0.5;
-  const defaultSpeed = "2s";
-  const defaultDirection = "-360deg";
-
+function CircularSpinner({
+  color = "#D97292",
+  opacity = 0.5,
+  speed = "2s",
+  direction = "-360deg",
+  size = "100",
+}) {
   return (
     <StyledSVG
       xmlns="http://www.w3.org/2000/svg"
-      width="110"
-      height="110"
+      width={size}
+      height={size}
       fill="none"
-      speed={speed || defaultSpeed}
-      direction={direction || defaultDirection}
+      speed={speed}
+      direction={direction}
+      viewBox="0 0 110 110"
     >
       <path
-        fill={color || defaultColor}
+        fill={color}
         d="M110 55c0 30.376-24.624 55-55 55S0 85.376 0 55 24.624 0 55 0s55 24.624 55 55Zm-92.85 0c0 20.904 16.946 37.85 37.85 37.85S92.85 75.904 92.85 55 75.904 17.15 55 17.15 17.15 34.096 17.15 55Z"
-        opacity={opacity || defaultOpacity}
+        opacity={opacity}
       />
       <path
-        fill={color || defaultColor}
+        fill={color}
         d="M101.425 55c4.736 0 8.643-3.862 7.908-8.54A54.999 54.999 0 0 0 63.54.667C58.862-.068 55 3.84 55 8.575c0 4.736 3.887 8.478 8.502 9.542a37.843 37.843 0 0 1 18.262 10.119 37.85 37.85 0 0 1 10.119 18.262C92.947 51.113 96.689 55 101.425 55Z"
       />
     </StyledSVG>
@@ -54,6 +56,7 @@ CircularSpinner.propTypes = {
   speed: PropTypes.string,
 
   direction: PropTypes.string,
+  size: PropTypes.string,
 };
 
 export default CircularSpinner;
--- src/components/DotSpinner.jsx
+++ src/components/DotSpinner.jsx
@@ -4,66 +4,66 @@ import PropTypes from "prop-types";
 import styled, { keyframes } from "styled-components";
 
 const jump = keyframes`
-  0%, 35%, 70%, 100% {
-    transform: translateY(0);
-  }
-  35% {
-    transform: translateY(-25px);
-  }
+	0%, 60%, 100% {
+		transform: initial;
+	}
+
+	30% {
+		transform: translateY(-15px);
+	}
+  
 `;
 const StyledSVG = styled.svg`
-  animation: ${jump} 2s linear infinite;
   color: ${(props) => props.color};
 `;
 
 const StyledCircle = styled.circle`
-  animation: ${jump} 4s linear infinite;
+  animation: ${jump} ${(props) => props.duration} 1.3s linear infinite;
+
   &:nth-child(1) {
     animation-delay: 0;
   }
   &:nth-child(2) {
-    animation-delay: 1s;
+    animation-delay: -1.1s;
   }
   &:nth-child(3) {
-    animation-delay: 2s;
+    animation-delay: -0.9s;
   }
 `;
 //delay between each child
-function DotSpinner({ colors = [] }) {
-  const defaultColors = ["#22333B", "#EAE0D5", "#C6AC8F"];
+function DotSpinner({
+  colors = ["#22333B", "#EAE0D5", "#C6AC8F"],
+  count = colors.length,
+
+  radius = "8",
+}) {
+  const dots = Array.from({ length: count });
 
   return (
     <StyledSVG
       xmlns="http://www.w3.org/2000/svg"
-      width="100"
+      width={count * 20 * 20}
       height="100"
       viewBox="0 0 100 100"
       fill="none"
     >
-      <StyledCircle
-        cx="23.5"
-        cy="49.5"
-        r="7.5"
-        fill={colors[0] || defaultColors[0]}
-      />
-      <StyledCircle
-        cx="49.5"
-        cy="49.5"
-        r="7.5"
-        fill={colors[1] || defaultColors[1]}
-      />
-      <StyledCircle
-        cx="75.5"
-        cy="49.5"
-        r="7.5"
-        fill={colors[2] || defaultColors[2]}
-      />
+      {dots.map((dot, index) => (
+        <StyledCircle
+          cx={(index + 1) * 20}
+          cy="49.5"
+          r={radius}
+          fill={colors[index]}
+          key={index}
+        />
+      ))}
     </StyledSVG>
   );
 }
 
 DotSpinner.propTypes = {
   colors: PropTypes.arrayOf(PropTypes.string),
+  duration: PropTypes.string,
+  width: PropTypes.number,
 };
 
 export default DotSpinner;
